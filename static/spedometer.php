a66beebacbc36da50fc0bc77aed699ca2a891332
Emiliano Ostellino <73698038+emioj89@users.noreply.github.com>
2021-11-23 15:17:39 +0100

    Update spedometer.php

--- static/spedometer.php
+++ static/spedometer.php
@@ -4,12 +4,12 @@ Class Speedometer {
     public const CONVERTKMTOMILES = 0.621;
     public const CONVERTMILESTOKM = 1.609;
 
-    public static function retrieveMiles(int $miles) : ?int
+    public static function retrieveMiles(int $miles) : float
     {
         return $miles * self::CONVERTKMTOMILES;
     }
 
-    public static function retrieveKm(int $km): ?int
+    public static function retrieveKm(int $km): float
     {
         return $km * self::CONVERTMILESTOKM;
     }
